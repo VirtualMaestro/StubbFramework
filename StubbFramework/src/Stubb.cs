f088428cd9f8c82bec59420483290b4a9d4ab831
VirtualMaestro <virtualmaestro@gmail.com>
2019-08-18 13:12:41 +0200

    Init commit

---
+++ StubbFramework/src/Stubb.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StubbFramework
+{
+    public class Stubb
+    {
+        private Stubb()
+        {}
+        
+        private static readonly Lazy<Stubb> _lazy = new Lazy<Stubb>(() => new Stubb());
+
+        public static Stubb Instance => _lazy.Value;
+    }
+}
