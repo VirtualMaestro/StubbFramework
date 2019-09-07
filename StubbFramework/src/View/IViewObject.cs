91e2ae63a5652ef43f4319bc6b4e094deed6d684
VirtualMaestro <virtualmaestro@gmail.com>
2019-09-07 17:27:52 +0200

    add basic interface and component for View

---
+++ StubbFramework/src/View/IViewObject.cs
@@ -0,0 +1,12 @@
+﻿using Leopotam.Ecs;
+
+namespace StubbFramework.View
+{
+    public interface IViewObject
+    {
+        string Name { get; }
+        EcsEntity Entity { get; }
+        void Dispose();
+        bool IsDisposed { get; }
+    }
+}
---
+++ StubbFramework/src/View/ViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Leopotam.Ecs;
+
+namespace StubbFramework.View
+{
+    public class ViewComponent : IEcsAutoResetComponent
+    {
+        public IViewObject View;
+        
+        public void Reset()
+        {
+            if (View.IsDisposed == false)
+            {
+                View.Dispose();
+                View = null;
+            }
+        }
+    }
+}
