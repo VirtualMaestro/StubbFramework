412fda83157e0d4fddf6c8d5e7fab5ececc90e89
VirtualMaestro <virtualmaestro@gmail.com>
2019-09-17 15:49:16 +0200

    fix Create invocation

--- StubbFramework/src/StubbContextDefault.cs
+++ StubbFramework/src/StubbContextDefault.cs
@@ -11,7 +11,6 @@ namespace StubbFramework
         
         public StubbContextDefault(IStubbDebug debug = null)
         {
-           Create();
            DebugInfo = debug;
         }
         
