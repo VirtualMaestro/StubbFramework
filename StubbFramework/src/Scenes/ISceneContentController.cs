70c9362d3ce988881852e375c41e07b8e4f6f5ae
VirtualMaestro <virtualmaestro@gmail.com>
2019-09-17 20:23:08 +0200

    add interfaces - ISceneController and ISceneContentController

---
+++ StubbFramework/src/Scenes/ISceneContentController.cs
@@ -0,0 +1,10 @@
+﻿namespace StubbFramework.Scenes
+{
+    public interface ISceneContentController
+    {
+        bool IsActive();
+        void Show();
+        void Hide();
+        void Destroy();
+    }
+}
---
+++ StubbFramework/src/Scenes/ISceneController.cs
@@ -0,0 +1,12 @@
+﻿namespace StubbFramework.Scenes
+{
+    public interface ISceneController
+    {
+        void Initialize();
+        void ShowContent();
+        void HideContent();
+        bool IsContentActive { get; }
+        string SceneName { get; }
+        void Destroy();
+    }
+}
