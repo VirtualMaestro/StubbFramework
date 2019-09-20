8248c42a804d9cceb09183477ccfc2ce735be1bf
VirtualMaestro <virtualmaestro@gmail.com>
2019-09-20 13:32:30 +0200

    add SceneComponent

---
+++ StubbFramework/src/Scenes/SceneComponent.cs
@@ -0,0 +1,14 @@
+﻿using Leopotam.Ecs;
+
+namespace StubbFramework.Scenes
+{
+    public class SceneComponent : IEcsAutoResetComponent
+    {
+        public ISceneController scene;
+        
+        public void Reset()
+        {
+            scene = null;
+        }
+    }
+}
