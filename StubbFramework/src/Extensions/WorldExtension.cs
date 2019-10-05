38737af1fa6e3386d2c1f22a88d383c7362da746
VirtualMaestro <virtualmaestro@gmail.com>
2019-10-05 18:27:58 +0200

    Renaming

--- StubbFramework/src/Extensions/WorldExtension.cs
+++ StubbFramework/src/Extensions/WorldExtension.cs
@@ -12,12 +12,12 @@ namespace StubbFramework.Extensions
         /// Add configuration of the scenes list to load.
         /// </summary>
         /// <param name="world"> Extension to the EcsWorld</param>
-        /// <param name="scenes">ISceneLoadingListConfig - list of the scenes configs</param>
+        /// <param name="config">Config of scenes to load</param>
         /// <param name="unloadScenes">scenes names which have to unload after given list config of new scenes will be loaded.</param>
-        public static void LoadScenes(this EcsWorld world, ILoadingScenesConfig scenes, string[] unloadScenes = null)
+        public static void LoadScenes(this EcsWorld world, ILoadingScenesConfig config, string[] unloadScenes = null)
         {
             world.NewEntityWith<LoadScenesComponent, InternalNewSceneListComponent>(out var loadScenes, out var newEntity);
-            loadScenes.Config = scenes;
+            loadScenes.Config = config;
             loadScenes.UnloadScenes = unloadScenes;
         }
 
