1ca766ace9e079b291adbc42ce6231bb35f9380b
VirtualMaestro <virtualmaestro@gmail.com>
2019-10-17 13:12:38 +0200

    Refactoring: Add logger class

---
+++ StubbFramework/src/Logging/log.cs
@@ -0,0 +1,47 @@
+﻿using DesperateDevs.Logging;
+
+namespace StubbUnity.Logging
+{
+    public static class log
+    {
+        public static void Trace(string message)
+        {
+            fabl.Trace(message);
+        }
+
+        public static void Debug(string message)
+        {
+            fabl.Debug(message);
+        }
+
+        public static void Info(string message)
+        {
+            fabl.Info(message);
+        }
+
+        public static void Warn(string message)
+        {
+            fabl.Warn(message);
+        }
+
+        public static void Error(string message)
+        {
+            fabl.Error(message);
+        }
+
+        public static void Fatal(string message)
+        {
+            fabl.Fatal(message);
+        }
+
+        public static void Assert(bool condition, string message)
+        {
+            fabl.Assert(condition, message);
+        }
+
+        public static void AddAppender(LogDelegate appender)
+        {
+            fabl.AddAppender(appender);
+        }
+    }
+}
