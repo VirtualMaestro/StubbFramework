1ed693c6d1d039cfa06ac1cea61d0c284bc5b5f6
VirtualMaestro <virtualmaestro@gmail.com>
2019-10-23 20:19:31 +0200

    Make NewSceneMarkerComponent as a one-frame-component

--- StubbFramework/src/Scenes/Components/NewSceneMarkerComponent.cs
+++ StubbFramework/src/Scenes/Components/NewSceneMarkerComponent.cs
@@ -5,6 +5,6 @@ namespace StubbFramework.Scenes.Components
     /// <summary>
     /// Will be added to every new created scene (Need only for bunch scene loading)
     /// </summary>
-    public class NewSceneMarkerComponent : IEcsIgnoreInFilter
+    public class NewSceneMarkerComponent : IEcsOneFrame, IEcsIgnoreInFilter
     {}
 }
