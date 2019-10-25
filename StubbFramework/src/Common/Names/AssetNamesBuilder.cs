fc22c63a4bf90b0a6e5dd2684e8a52b473971ccf
VirtualMaestro <virtualmaestro@gmail.com>
2019-10-25 17:42:01 +0200

    Fix: interfaces agreement

--- StubbFramework/src/Common/Names/AssetNamesBuilder.cs
+++ StubbFramework/src/Common/Names/AssetNamesBuilder.cs
@@ -4,18 +4,18 @@ namespace StubbFramework.Common.Names
 {
     public class AssetNamesBuilder<T> where T: IAssetName, new()
     {
-        private readonly List<T> _names;
+        private readonly List<IAssetName> _names;
         
-        public List<T> Build => _names;
+        public List<IAssetName> Build => _names;
         
         public AssetNamesBuilder()
         {
-            _names = new List<T>();
+            _names = new List<IAssetName>();
         }
 
         public AssetNamesBuilder<T> Add(string name, string path = null)
         {
-            T assetName = new T();
+            IAssetName assetName = new T();
             assetName.Set(name, path);
             _names.Add(assetName);
             
