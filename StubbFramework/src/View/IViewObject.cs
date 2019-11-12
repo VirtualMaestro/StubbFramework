a76a8f7d930d33ae8e1928915e3d37d4a0eb60d2
VirtualMaestro <virtualmaestro@gmail.com>
2019-11-12 11:02:57 +0100

    add World getter to the IViewObject

--- StubbFramework/src/View/IViewObject.cs
+++ StubbFramework/src/View/IViewObject.cs
@@ -1,9 +1,14 @@
-﻿using StubbFramework.Common;
+﻿using Leopotam.Ecs;
+using StubbFramework.Common;
 
 namespace StubbFramework.View
 {
     public interface IViewObject : IEntityContainer, IDispose
     {
         string Name { get; }
+        /// <summary>
+        /// An instance of the World where this IViewObject belongs to.
+        /// </summary>
+        EcsWorld World { get; }
     }
 }
