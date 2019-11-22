4ead3bf617edbf7f1913940079ef0ae6cabb69ad
VirtualMaestro <virtualmaestro@gmail.com>
2019-11-22 16:30:11 +0100

    Small fix

--- StubbFramework/src/Physics/Components/CleanupCollisionComponent.cs
+++ StubbFramework/src/Physics/Components/CleanupCollisionComponent.cs
@@ -1,5 +1,9 @@
 ﻿namespace StubbFramework.Physics.Components
 {
-    internal sealed class CleanupCollisionComponent
+    /// <summary>
+    /// For internal use.
+    /// It is added to all collision component automatically (e.g. CollisionEnterComponent) in order to be destroyed in CleanupCollisionSystem.
+    /// </summary>
+    public sealed class CleanupCollisionComponent
     {}
 }
