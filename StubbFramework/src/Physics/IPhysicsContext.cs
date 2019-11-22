90707de613d9038aec65321eb6176c75d76c24e7
VirtualMaestro <virtualmaestro@gmail.com>
2019-11-22 21:03:00 +0100

    Add physics context interface

---
+++ StubbFramework/src/Physics/IPhysicsContext.cs
@@ -0,0 +1,9 @@
+﻿namespace StubbFramework.Physics
+{
+    /// <summary>
+    /// Context which is implemented this interface will be processed only on PhysicsUpdate loop (e.g. for Unity FixedUpdate).
+    /// </summary>
+    public interface IPhysicsContext : IStubbContext
+    {
+    }
+}
