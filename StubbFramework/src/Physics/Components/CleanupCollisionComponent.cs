6293c10297de8b643978d68ec21c24b68f87e909
VirtualMaestro <virtualmaestro@gmail.com>
2019-11-23 10:43:10 +0100

    Small optimization

--- StubbFramework/src/Physics/Components/CleanupCollisionComponent.cs
+++ StubbFramework/src/Physics/Components/CleanupCollisionComponent.cs
@@ -1,9 +1,11 @@
-﻿namespace StubbFramework.Physics.Components
+﻿using Leopotam.Ecs;
+
+namespace StubbFramework.Physics.Components
 {
     /// <summary>
     /// For internal use.
     /// It is added to all collision component automatically (e.g. CollisionEnterComponent) in order to be destroyed in CleanupCollisionSystem.
     /// </summary>
-    public sealed class CleanupCollisionComponent
+    public sealed class CleanupCollisionComponent : IEcsIgnoreInFilter
     {}
 }
