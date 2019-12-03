b6faa7a7ae4a722127a9bdf9279b49a99c7737c9
VirtualMaestro <virtualmaestro@gmail.com>
2019-12-03 10:25:18 +0100

    fix log for the new logger

--- StubbFramework/src/Logging/log.cs
+++ StubbFramework/src/Logging/log.cs
@@ -1,55 +1,62 @@
 ﻿using System.Diagnostics;
-using DesperateDevs.Logging;
+using StubbLog.StubbLog;
 
 namespace StubbFramework.Logging
 {
     public static class log
     {
+        private static readonly Logger _log;
+
+        static log()
+        {
+            _log = LoggerManager.Create("StubbLogger");
+        }
+
         [Conditional("DEBUG")]
         public static void Trace(string message)
         {
-            fabl.Trace(message);
+            _log.Trace(message);
         }
 
         [Conditional("DEBUG")]
         public static void Debug(string message)
         {
-            fabl.Debug(message);
+            _log.Debug(message);
         }
 
         [Conditional("DEBUG")]
         public static void Info(string message)
         {
-            fabl.Info(message);
+            _log.Info(message);
         }
 
         [Conditional("DEBUG")]
         public static void Warn(string message)
         {
-            fabl.Warn(message);
+            _log.Warn(message);
         }
 
         [Conditional("DEBUG")]
         public static void Error(string message)
         {
-            fabl.Error(message);
+            _log.Error(message);
         }
 
         [Conditional("DEBUG")]
         public static void Fatal(string message)
         {
-            fabl.Fatal(message);
+            _log.Fatal(message);
         }
 
         [Conditional("DEBUG")]
         public static void Assert(bool condition, string message)
         {
-            fabl.Assert(condition, message);
+            _log.Assert(condition, message);
         }
 
-        public static void AddAppender(LogDelegate appender)
+        public static void AddAppender(Logger.LogDelegate appender)
         {
-            fabl.AddAppender(appender);
+            LoggerManager.Add(appender);
         }
     }
 }
