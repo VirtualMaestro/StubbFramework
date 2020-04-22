946091a26341292f91c76f567135bd3272124b1f
VirtualMaestro <virtualmaestro@gmail.com>
2020-04-22 09:17:10 +0200

    Add NumContexts

--- StubbFramework/src/Stubb.cs
+++ StubbFramework/src/Stubb.cs
@@ -4,13 +4,15 @@ namespace StubbFramework
 {
     public static class Stubb
     {
-        private static int _index = 0;
+        private static int _index;
         private static IStubbContext[] _contexts = new IStubbContext[10];
 
         public static void AddContext(IStubbContext context)
         {
             _contexts[_index++] = context;
         }
+
+        public static int NumContexts => _contexts.Length;
 
         [MethodImpl (MethodImplOptions.AggressiveInlining)]
         public static IStubbContext GetContext(int index = 0)
