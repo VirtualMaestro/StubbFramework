735edd93bce2899e37c2ece81f403b4ae47836bf
VirtualMaestro <virtualmaestro@gmail.com>
2020-04-23 07:07:00 +0200

    Refactoring: remove content controller and merge ISceneContent into ISceneController

--- StubbFramework/src/Scenes/ISceneController.cs
+++ StubbFramework/src/Scenes/ISceneController.cs
@@ -7,13 +7,34 @@ namespace StubbFramework.Scenes
     /// Interface for the SceneController which should be implemented in engine specific class.
     /// It will contain original Scene specific engine instance .
     /// </summary>
-    public interface ISceneController : ISceneContent, IEntityContainer
+    public interface ISceneController : IEntityContainer
     {
+        /// <summary>
+        /// Initialization code here.
+        /// </summary>
         void Initialize();
         IAssetName SceneName { get; }
         bool IsDestroyed { get; }
+        /// <summary>
+        /// Check if a scene is main.
+        /// </summary>
         bool IsMain { get; }
+        /// <summary>
+        /// Set a scene as main.
+        /// </summary>
         void SetAsMain();
+        /// <summary>
+        /// For internal use. For showing scene use World.ActivateSceneByName() or World.ActivateScene().
+        /// </summary>
+        void ShowContent();
+        /// <summary>
+        /// For internal use. For hiding scene use World.DeactivateSceneByName() or World.DeactivateScene().
+        /// </summary>
+        void HideContent();
+        /// <summary>
+        /// Determines if scene is active.
+        /// </summary>
+        bool IsContentActive { get; }
         void Destroy();
     }
 }
