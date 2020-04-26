1382749739e85da09bf94cde27cc8c6b55f7a0aa
VirtualMaestro <virtualmaestro@gmail.com>
2020-04-26 22:11:38 +0200

    fix

--- StubbFramework/src/Debugging/StubbDebug.cs
+++ StubbFramework/src/Debugging/StubbDebug.cs
@@ -7,14 +7,14 @@ namespace StubbFramework.Debugging
 #if DEBUG
         Leopotam.Ecs.RemoteDebug.RemoteDebugClient _debug;
 #endif
-        public void Init(EcsSystems rootSystems, EcsWorld world)
+        public virtual void Init(EcsSystems rootSystems, EcsWorld world)
         {
 #if DEBUG
             _debug = new Leopotam.Ecs.RemoteDebug.RemoteDebugClient (world);
 #endif 
         }
 
-        public void Debug()
+        public virtual void Debug()
         {
 #if DEBUG
             _debug?.Run ();
